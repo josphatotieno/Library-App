b39878b8209331dc32eb0eb4556db769217ae448
Josphat Otieno <josphathumphrey5@gmail.com>
2022-09-14 11:45:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const title = document.querySelector('#title');
 const numberOfPages = document.querySelector('#number-of-pages');
 const statusOfReading = document.querySelector('#read');
 const btn = document.querySelector('.btn');
-const bookItems = document.querySelector('.book-items')
+const bookWrapper = document.querySelector('.books-wrapper');
+const deleteBtn = document.querySelector('.delete');
 
 btn.addEventListener('click', () => {
     if(author.value !== '' && title.value !== '' && numberOfPages.value !== '' && statusOfReading.value !== '') {
@@ -29,18 +30,32 @@ function addBookLibrary() {
 }
 
 function displayBookToUI() {
-    myLibrary.forEach(book => {
-        
-        bookItems.innerHTML += `
-        <div class="book-item">
-            <h3>${book.title}</h3>
-            <p>by ${book.author}</p>
-            <p>${book.pages}</p>
-            <p>${book.read}</p>
-        </div>
-        `
+    console.log(myLibrary);
+    myLibrary.forEach((book,index) => {
+        if(index === myLibrary.length -1) {
+            document.querySelector('tbody').innerHTML += `
+                <tr>
+                    <td>${book.author}</td>
+                    <td>${book.title}</td>
+                    <td>${book.pages}</td>
+                    <td>${book.author}</td>
+                    <td>${book.read}</td>
+                    <td class="delete" data-index=${index}>Delete</td>
+                </tr>
+            `
+        }
     })
 }
+
+bookWrapper.addEventListener('click', (e) => {
+    if(e.target.classList.contains('delete')) {
+        myLibrary.splice(e.target.dataset.index,1);
+        e.target.parentElement.remove();
+    }
+    
+})
+
+
 
 function clearFields() {
     author.value = '';
